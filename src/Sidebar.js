fb6c28c1e6226efb4396241ff515bcf116a4ac1f
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-04-10 14:08:46 +0500

    new commit

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,30 +1,31 @@
-import React from 'react'
-import { Avatar } from '@mui/material'
-import DonutLargeIcon from "@mui/icons-material/DonutLarge"
-import ChatIcon from "@mui/icons-material/Chat"
-import MoreVertIcon from "@mui/icons-material/MoreVert"
-
-
-import "./Sidebar.css"
+import React from "react";
+import { Avatar, IconButton } from "@mui/material";
+import "./Sidebar.css";
+import DonutLargeIcon from "@mui/icons-material/DonutLarge";
+import ChatIcon from "@mui/icons-material/Chat";
+import MoreVertIcon from "@mui/icons-material/MoreVert";
 
 function Sidebar() {
   return (
-    <div className='sidebar'>
-    <div className="sidebar__header">
+    <div className="sidebar">
+      <div className="sidebar__header">
         <Avatar />
         <div className="sidebar__headerRight">
-           <DonutLargeIcon />
-           <ChatIcon/>
-            <MoreVertIcon/>
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+           <IconButton>
+          <ChatIcon />
+          </IconButton>
+          <IconButton>
+          <MoreVertIcon />
+          </IconButton>
         </div>
-
+      </div>
+      <div className="sidebar__search"></div>
+      <div className="sidebar__chats"></div>
     </div>
-    <div className="sidebar__search">
-
-    </div>
-    <div className="sidebar__chats"></div>
-    </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
