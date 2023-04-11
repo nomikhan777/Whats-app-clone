3fb48f42796e38b3f55cdd24e467cd65dedbd40f
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-04-11 11:04:33 +0500

    new commit

--- src/Chat.js
+++ src/Chat.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import "./Chat.css"
-import { Avatar } from '@mui/material'
+import { Avatar , IconButton} from '@mui/material'
 import { useState , useEffect } from 'react';
+
+import { SearchOutlined , MoreVert, AttachFile} from '@mui/icons-material';
 
 function Chat() {
     const [seed, setSeed] = useState("");
@@ -21,6 +23,18 @@ function Chat() {
             <div className="chat__headerInfo">
                 <h3>Room Name</h3>
                 <p>Last seen at ...</p>
+            </div>
+
+            <div className="char__headerRight">
+            <IconButton>
+            <SearchOutlined />
+          </IconButton>
+          <IconButton>
+            <AttachFile />
+          </IconButton>
+          <IconButton>
+            <MoreVert />
+          </IconButton>
             </div>
 
         </div>
