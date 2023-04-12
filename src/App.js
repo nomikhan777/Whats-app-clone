87cec0797fea605c9606eb2bfdf9a8bd66147a5b
nomikhan777 <muhammadnomanbrw123@gmail.com>
2023-04-12 14:32:01 +0500

    new commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,28 @@
-import './App.css';
-import Chat from './Chat';
-import Sidebar from './Sidebar';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { useState } from "react";
+import "./App.css";
+import Chat from "./Chat";
+import Sidebar from "./Sidebar";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from './Login';
+import { useStateValue } from './StateProvider';
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="app">
-
-      <div className="app__body">
-      <Router>
-              <Sidebar />
-              <Routes>
-                <Route path='/' element={<></>} />
-                <Route path='/rooms/:roomId' element={<Chat />} />
-              </Routes>
-            </Router>
-            </div>
-
-
-
-
+      {!user ? (
+        <Login/>
+      ) : (
+        <div className="app__body">
+          <Router>
+            <Sidebar />
+            <Routes>
+              <Route path="/" element={<></>} />
+              <Route path="/rooms/:roomId" element={<Chat />} />
+            </Routes>
+          </Router>
+        </div>
+      )}
     </div>
   );
 }
---
+++ src/Login.js
@@ -0,0 +1,38 @@
+import { Button } from '@mui/material'
+import React from 'react'
+import { auth, provider } from './firebase'
+import { signInWithPopup } from 'firebase/auth'
+import './Login.css'
+import { useStateValue } from './StateProvider'
+import { actionTypes } from './ reducer'
+
+function Login() {
+    const [, dispatch] = useStateValue();
+
+    // Function to Sign In with google
+    const signIn = () => {
+        signInWithPopup(auth, provider).then(result => {
+            console.log(result)
+            dispatch({
+                type : actionTypes.SET_USER,
+                user : result.user
+            })
+        }).catch(error => {
+            alert(error.message);
+        })
+    }
+
+    return (
+        <div className='login'>
+            <div className='login__container'>
+                <img src='https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg' alt='' />
+                <div className='login__text'>
+                    <h1>Sign in to WhatsApp</h1>
+                </div>
+                <Button onClick={signIn}>Sign In with google</Button>
+            </div>
+        </div>
+    )
+}
+
+export default Login
